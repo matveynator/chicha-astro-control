c16bead1fe8f63d113de97fb919e8773b032c9a3
Matvey Gladkikh <matvey@copters.ru>
2026-04-10 12:19:29 +0300

    Fix Windows GPIO backend to use ECX1K.dll API

---
+++ pkg/gpio/gpio_windows.go
@@ -0,0 +1,185 @@
+//go:build windows
+
+package gpio
+
+import (
+	"fmt"
+	"sync/atomic"
+	"unsafe"
+
+	"golang.org/x/sys/windows"
+)
+
+const (
+	vecowInitIsolateNonIsolated        = 0
+	vecowInitDIONPN                    = 0
+	vecowGPIOConfigMask         uint16 = 0xFF00
+)
+
+type windowsAdapter struct {
+	dll         *windows.LazyDLL
+	procInitial *windows.LazyProc
+	procConfig  *windows.LazyProc
+	procGetGPIO *windows.LazyProc
+	procSetGPIO *windows.LazyProc
+
+	outputMask atomic.Uint32
+}
+
+func DefaultInputTemplate() string {
+	return ""
+}
+
+func DefaultOutputTemplate() string {
+	return ""
+}
+
+func Open(config Config) (Adapter, RuntimeMode, error) {
+	_ = config
+	adapter, err := openWindowsAdapter()
+	if err != nil {
+		return nil, RuntimeMode{}, err
+	}
+	return adapter, RuntimeMode{}, nil
+}
+
+func openWindowsAdapter() (*windowsAdapter, error) {
+	dll := windows.NewLazyDLL("ECX1K.dll")
+	if err := dll.Load(); err != nil {
+		return nil, fmt.Errorf("load ECX1K.dll: %w", err)
+	}
+
+	adapter := &windowsAdapter{
+		dll:         dll,
+		procInitial: dll.NewProc("Initial"),
+		procConfig:  dll.NewProc("SetGPIOConfig"),
+		procGetGPIO: dll.NewProc("GetGPIO"),
+		procSetGPIO: dll.NewProc("SetGPIO"),
+	}
+
+	if err := adapter.procInitial.Find(); err != nil {
+		_ = releaseLazyDLL(adapter.dll)
+		return nil, fmt.Errorf("resolve Initial: %w", err)
+	}
+	if err := adapter.procConfig.Find(); err != nil {
+		_ = releaseLazyDLL(adapter.dll)
+		return nil, fmt.Errorf("resolve SetGPIOConfig: %w", err)
+	}
+	if err := adapter.procGetGPIO.Find(); err != nil {
+		_ = releaseLazyDLL(adapter.dll)
+		return nil, fmt.Errorf("resolve GetGPIO: %w", err)
+	}
+	if err := adapter.procSetGPIO.Find(); err != nil {
+		_ = releaseLazyDLL(adapter.dll)
+		return nil, fmt.Errorf("resolve SetGPIO: %w", err)
+	}
+
+	if err := adapter.callInitial(); err != nil {
+		_ = releaseLazyDLL(adapter.dll)
+		return nil, err
+	}
+	if err := adapter.callSetGPIOConfig(vecowGPIOConfigMask); err != nil {
+		_ = releaseLazyDLL(adapter.dll)
+		return nil, err
+	}
+	if err := adapter.callSetGPIO(0); err != nil {
+		_ = releaseLazyDLL(adapter.dll)
+		return nil, err
+	}
+	adapter.outputMask.Store(0)
+
+	return adapter, nil
+}
+
+func (adapter *windowsAdapter) ReadInput(channel int) (bool, error) {
+	if channel < 1 || channel > InputCount {
+		return false, fmt.Errorf("invalid input channel %d", channel)
+	}
+
+	var state uint16
+	if err := adapter.callGetGPIO(&state); err != nil {
+		return false, err
+	}
+	bitMask := uint16(1 << (channel - 1))
+	return state&bitMask != 0, nil
+}
+
+func (adapter *windowsAdapter) WriteOutput(channel int, high bool) error {
+	if channel < 1 || channel > OutputCount {
+		return fmt.Errorf("invalid output channel %d", channel)
+	}
+
+	bitMask := uint32(1 << uint(channel+7))
+	for {
+		currentMask := adapter.outputMask.Load()
+		nextMask := currentMask
+		if high {
+			nextMask |= bitMask
+		} else {
+			nextMask &^= bitMask
+		}
+
+		if !adapter.outputMask.CompareAndSwap(currentMask, nextMask) {
+			continue
+		}
+
+		if err := adapter.callSetGPIO(uint16(nextMask)); err != nil {
+			adapter.outputMask.Store(currentMask)
+			return err
+		}
+		return nil
+	}
+}
+
+func (adapter *windowsAdapter) Close() error {
+	if adapter.dll == nil {
+		return nil
+	}
+	if err := releaseLazyDLL(adapter.dll); err != nil {
+		return fmt.Errorf("release ECX1K.dll: %w", err)
+	}
+	adapter.dll = nil
+	return nil
+}
+
+func releaseLazyDLL(dll *windows.LazyDLL) error {
+	if dll == nil {
+		return nil
+	}
+	handle := dll.Handle()
+	if handle == 0 {
+		return nil
+	}
+	return windows.FreeLibrary(windows.Handle(handle))
+}
+func (adapter *windowsAdapter) callInitial() error {
+	result, _, _ := adapter.procInitial.Call(vecowInitIsolateNonIsolated, vecowInitDIONPN)
+	if result != 0 {
+		return fmt.Errorf("Initial returned %d", result)
+	}
+	return nil
+}
+
+func (adapter *windowsAdapter) callSetGPIOConfig(mask uint16) error {
+	result, _, _ := adapter.procConfig.Call(uintptr(mask))
+	if result != 0 {
+		return fmt.Errorf("SetGPIOConfig returned %d", result)
+	}
+	return nil
+}
+
+func (adapter *windowsAdapter) callSetGPIO(mask uint16) error {
+	result, _, _ := adapter.procSetGPIO.Call(uintptr(mask))
+	if result != 0 {
+		return fmt.Errorf("SetGPIO returned %d", result)
+	}
+	return nil
+}
+
+func (adapter *windowsAdapter) callGetGPIO(state *uint16) error {
+	result, _, _ := adapter.procGetGPIO.Call(uintptr(unsafe.Pointer(state)))
+	if result != 0 {
+		return fmt.Errorf("GetGPIO returned %d", result)
+	}
+	return nil
+}
